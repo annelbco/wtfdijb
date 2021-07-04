079701436c933ac5a85cf1936bc6047fe53a021e
Anne Barrios <commits.noreply@annelb.com>
2021-07-05 00:03:42 +0800

    feat: init file upload

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,56 @@
 <template lang="pug">
-div
+div Tesseract
+    form(action='#')
+        input(type='file', name='', @change='getFile($event)')
 </template>
 
 <script>
+import { createWorker } from 'tesseract.js'
+import path from 'path'
+
 export default {
     name: 'App',
     components: {},
+    data() {
+        return {
+            file: '',
+            reader: '',
+            worker: '',
+        }
+    },
+
+    mounted() {
+        this.worker = createWorker({
+            langPath: path.join(__dirname, '..', 'lang-data'),
+            logger: m => console.log(m),
+        })
+    },
+
+    methods: {
+        async getFile(event) {
+            this.file = event.target.files[0]
+            this.reader = new FileReader()
+            this.reader.readAsText(this.file)
+            console.log(this.file)
+
+            // Tesseract.recognize(this.file.name, 'eng', {
+            //     logger: m => console.log(m),
+            // }).then(({ data: { text } }) => {
+            //     console.log(text)
+            // })
+
+            await this.worker.load()
+            await this.worker.loadLanguage('eng')
+            await this.worker.initialize('eng')
+            const {
+                data: { text },
+            } = await this.worker.recognize(
+                path.join(__dirname, '..', 'images', this.file.name)
+            )
+            console.log(text)
+            await this.worker.terminate()
+        },
+    },
 }
 </script>
 
